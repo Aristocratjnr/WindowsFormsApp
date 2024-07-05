05293c6b8f82b262f20825222c827801a6dc6783
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-05 09:21:26 +0000

    refactor: Add login and signup functionality to Form1

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,56 @@
-namespace WindowsFormsApp;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace WindowsFormsApp
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        // Variables for the drawing canvas
+        private bool isDrawing = false;
+        private Point lastPoint = Point.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            MessageBox.Show($"Username: {username}\nPassword: {password}");
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string phone = txtPhone.Text;
+            MessageBox.Show($"Name: {name}\nEmail: {email}\nPhone: {phone}");
+        }
+
+        private void pnlCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            lastPoint = e.Location;
+        }
+
+        private void pnlCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = pnlCanvas.CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, lastPoint, e.Location);
+                }
+                lastPoint = e.Location;
+            }
+        }
+
+        private void pnlCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
     }
 }
