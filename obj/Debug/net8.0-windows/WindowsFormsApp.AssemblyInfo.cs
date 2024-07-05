22c6e1be8fa137902a237e53b457987af6877940
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-05 12:01:28 +0000

    refactor: Update AssemblyInfo.cs with new informational version

--- obj/Debug/net8.0-windows/WindowsFormsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WindowsFormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindowsFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1caa8979ee1961f1f03d27d00080b6c011c19ce0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485d691572c5ff4ea2b7e4ae11f51dd315fc796e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindowsFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindowsFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
