d4b1acc192df62e2413946b9a8c1922a668d23c6
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-05 09:22:24 +0000

    refactor: Update Program.cs to enable visual styles and set compatible text rendering

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
-namespace WindowsFormsApp;
+using System;
+using System.Windows.Forms;
 
-static class Program
+namespace WindowsFormsApp
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    static class Program
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
-    }    
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
 }
