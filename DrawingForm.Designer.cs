22ba9934203c75d44a2697f6d0dcfd2545945f4e
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-05 10:17:33 +0000

    refactor: Update DrawingForm.Designer.cs to add drawing canvas

---
+++ DrawingForm.Designer.cs
@@ -0,0 +1,45 @@
+namespace ProfessionalWindowsFormsApp
+{
+    partial class DrawingForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pnlCanvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlCanvas
+            // 
+            this.pnlCanvas.BackColor = System.Drawing.Color.White;
+            this.pnlCanvas.Location = new System.Drawing.Point(12, 12);
+            this.pnlCanvas.Name = "pnlCanvas";
+            this.pnlCanvas.Size = new System.Drawing.Size(760, 537);
+            this.pnlCanvas.TabIndex = 0;
+            this.pnlCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlCanvas_MouseDown);
+            this.pnlCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlCanvas_MouseMove);
+            this.pnlCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlCanvas_MouseUp);
+            // 
+            // DrawingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pnlCanvas);
+            this.Name = "DrawingForm";
+            this.Text = "Drawing Canvas";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Panel pnlCanvas;
+    }
+}
