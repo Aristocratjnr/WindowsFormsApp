f6a811a6a6121908f4c049443dfcf1c56d948b21
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-05 10:18:05 +0000

    refactor: Update Program.cs to use LoginForm instead of Form1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Windows.Forms;
-using ProfessionalWindowsFormsApp;
 
-//This is the main entry point for the application.
-
-namespace WindowsFormsApp
+namespace ProfessionalWindowsFormsApp
 {
     static class Program
     {
@@ -13,7 +10,7 @@ namespace WindowsFormsApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
