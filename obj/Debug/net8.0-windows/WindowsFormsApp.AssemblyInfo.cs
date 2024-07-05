6bcf666a4afae044684e2f404f99db8dba36dc0a
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-07-05 09:27:32 +0000

    refactor: Update Program.cs to enable visual styles and set compatible text rendering

--- obj/Debug/net8.0-windows/WindowsFormsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WindowsFormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindowsFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20d318421aed47c25c342b8093370d7357a5bcf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b1acc192df62e2413946b9a8c1922a668d23c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindowsFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindowsFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
